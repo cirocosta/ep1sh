403d9d47f08b6ce72391b093c0c3683933db2ee1
Ciro S. Costa <ciro9758@gmail.com>
2015-08-11 15:45:21 -0300

    updates debug w/ str formatting

--- src/debug.h
+++ src/debug.h
@@ -1,28 +1,27 @@
 #ifndef EP1SH__DEBUG_H
 #define EP1SH__DEBUG_H
 
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define LOG(message)                                                           \
+#define LOG(message, ...)                                                      \
   do {                                                                         \
     fprintf(stdout, "\n(LOG)\t");                                              \
-    fprintf(stdout, message);                                                  \
+    fprintf(stdout, message, ##__VA_ARGS__);                                   \
   } while (0)
 
-#define LOGERR(message)                                                        \
+#define LOGERR(message, ...)                                                   \
   do {                                                                         \
     fprintf(stderr, "\n(LOGERR)\t");                                           \
-    fprintf(stderr, message);                                                  \
+    fprintf(stderr, message, ##__VA_ARGS__);                                   \
   } while (0)
 
-#define ASSERT(condition, message)                                             \
+#define ASSERT(condition, message, ...)                                        \
   do {                                                                         \
     if (!(condition)) {                                                        \
       fprintf(stderr, "\n" __BASE_FILE__ "%2d\n", __LINE__);                   \
       fprintf(stderr, "Assertion `%s` failed\n", #condition);                  \
-      fprintf(stderr, "\t" message "\n");                                      \
+      fprintf(stderr, "\t" message "\n", ##__VA_ARGS__);                       \
       exit(EXIT_FAILURE);                                                      \
     }                                                                          \
   } while (0)
